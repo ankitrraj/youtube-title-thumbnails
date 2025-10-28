8325924febca8bc88463d2f965344bfb90538ca1
ankitrraj <ankitrajoria81@gmail.com>
2025-10-28 23:47:27 +0530

    complete the first api

--- steps/first-api.step.ts
+++ steps/first-api.step.ts
@@ -27,6 +27,47 @@ export const handler =async (req:any ,{emit ,logger,state}:any )=>{
             };
         }
 
+        //validate 
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            return{
+                status:400,
+                body:{
+                error : "invalid email address"
+            },
+            };
+        }
+
+        const jobId =`job_${Date.now()}_${Math.random().toString(36).substring(2,9)}`;
+
+        await state.set(`job : ${jobId}`,{
+            jobId,
+            channel,
+            email,
+            state : "queue",
+            createdAt : new Date().toISOString(),
+
+        })
+        logger.info('job created', {jobId ,channel,email})
+        await emit({
+            topic : "youtube.title",
+            data : {
+                jobId,
+                channel,
+                email
+
+            }
+        })
+        return{
+            status:202,
+            body:{
+                succes : true,
+                jobId,
+                message : "job created and queued for processing"
+            }
+        }
+        
+
     } catch (error:any) {
         logger.error('error in submissions{error:error.message}')
         return{
